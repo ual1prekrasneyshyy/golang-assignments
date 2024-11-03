25aa2b79f6e966d36226b6fe0c964571a96c599c
l10n <uali03.alm@gmail.com>
2024-11-03 23:56:48 +0500

    Assignment 3

--- assignment_3/front/MyProject/src/app/app.routes.ts
+++ assignment_3/front/MyProject/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {ItemComponent} from './components/item/item.component';
+import {ItemDetailComponent} from './components/item-detail/item-detail.component';
+import {AddItemComponent} from './components/add-item/add-item.component';
+import {UpdateItemComponent} from './components/update-item/update-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ItemComponent},
+    {path: 'item/:id', component: ItemDetailComponent},
+    {path: 'add-item', component: AddItemComponent},
+    {path: 'update-item/:id', component: UpdateItemComponent},
+];
